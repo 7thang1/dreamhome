0c2d6d62ce1bdb6719a930bd09fa61b8aeffc6a3
Thinh-Tan Thi <thinhprocf5879@gmail.com>
2023-11-06 14:27:30 +0700

    project stucture change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 /** @type {import('tailwindcss').Config} */
-module.exports =withMT({
+module.exports = withMT({
   content: [
-
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -13,14 +13,13 @@ module.exports =withMT({
         primary: "#806056",
       },
       backgroundImage: {
-        'map': "url('/map.svg')",
-        'hcm': "url('/hcm.jpg')",
-        'dn': "url('/dn.jpg')",
-        'hn': "url('/hn.jpg')",
-        'bd': "url('/bd.jpg')",
-      }
+        map: "url('/map.svg')",
+        hcm: "url('/hcm.jpg')",
+        dn: "url('/dn.jpg')",
+        hn: "url('/hn.jpg')",
+        bd: "url('/bd.jpg')",
+      },
     },
   },
-  plugins: [
- ],
+  plugins: [],
 });
